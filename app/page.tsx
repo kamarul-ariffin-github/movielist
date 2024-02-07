25d7dd4accb54be5e8d73470ff2b0b01bedce63d
kamarul-ariffin-github <mkamarulariffinrosli@gmail.com>
2024-02-07 17:22:26 +0800

    designing process

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,28 @@
-import Image from "next/image";
+import Header from "@/components/header";
 
 export default function Home() {
-  return <div>test</div>;
+  return (
+    <>
+      <Header />
+      <div className=" p-2 flex flex-row gap-5">
+        <div className="border-2 p-2">
+          <p>FILTER BY</p>
+          <p>Genres</p>
+          <p>Ratings</p>
+        </div>
+        <div className="flex flex-1 flex-row flex-wrap gap-3">
+          <div className=" shadow-md border p-2 hover:scale-110 transform transition duration-300">
+            <p className=" px-6 py-10">Image</p>
+            <p>Title</p>
+            <p>Release date</p>
+          </div>
+          <div className=" shadow-md border p-2 hover:scale-110 transform transition duration-300">
+            <p className=" px-6 py-10">Image</p>
+            <p>Title</p>
+            <p>Release date</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
---
+++ components/header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <>
+      <div className=" bg-sky-300 pt-8 shadow-md">
+        <div className=" bg-white">
+          <p className=" text-sky-400 font-bold text-3xl py-2 px-2">
+            Movie List
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
