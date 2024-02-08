d277b310fadde2a0274cb4646b265e11bd2fc385
kamarul-ariffin-github <mkamarulariffinrosli@gmail.com>
2024-02-08 13:53:00 +0800

    filter,sort,modal,paging

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
     <>
-      <div className=" bg-sky-300 pt-8 shadow-md">
-        <div className=" bg-white">
+      <div className=" bg-sky-300 pt-8 shadow-md w-full">
+        <div className=" bg-white w-full">
           <p className=" text-sky-400 font-bold text-3xl py-2 px-2">
             Popcorn Time
           </p>
